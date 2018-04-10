9c444413832a2afb71b33abba7fbf254c3d3ca29
Maxwell Carlson <bart4282819@gmail.com>
2018-04-10 00:59:14 -0700

    Tiny bit of work to SmallVector class.

--- Containers/SmallVec.h
+++ Containers/SmallVec.h
@@ -5,12 +5,25 @@
 template<class Type, int size, class Allocator = std::allocator<Type>>
 class SmallVec
 {
-
+public:
 	using AlTraits = std::allocator_traits<Allocator>;
 	using Storage = typename std::aligned_storage<sizeof(Type), alignof(Type)>::type;
+	using size_type = size_t;
 
-
+	using NodePtr = Type *;
 
 private:
+	Allocator alloc;
+
 	Storage data[size];
+
+	size_type MySize;
+
+	NodePtr MyBegin;
+	NodePtr MyEnd;
+	
+	void grow()
+	{
+		const size_type newSize = MySize + (MySize + 4) / 2; // TODO: Revisit this
+	}
 };
