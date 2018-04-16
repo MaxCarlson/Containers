ae6440e06934442919d5134cd39f71f6812e5304
Maxwell Carlson <bart4282819@gmail.com>
2018-04-16 00:09:20 -0700

    Bug with FlatTree's Set seems to be fixed. std::conditional_t in FlatTree resulted in set trying to use a pair

--- Containers/FlatTree.h
+++ Containers/FlatTree.h
@@ -54,26 +54,23 @@ class FlatTree
 public:
 	using MyBase = FlatTree<Traits>;
 
-	using vtype = typename Traits::value_type;
-	using ktypet = typename Traits::key_type;
-	using ktype = typename std::remove_cv<ktypet>::type;
+	using value_type = typename Traits::value_type;
+	using key_type   = typename Traits::key_type;
 
-	using NodeType = std::conditional_t<std::is_same_v<vtype, ktypet>, std::pair<ktype, vtype>, typename Traits::key_type>;
+	using NodeType = std::conditional_t<std::is_same_v<typename Traits::node_type, const key_type>, 
+		typename Traits::key_type,
+		std::pair<key_type, value_type>>;
 
 	using BaseTypes = OrderedTypesNoNodeWrapper<Traits, NodeType>;
-	//using BaseTypes = OrderedTypes<Traits, FlatTreeNode>;
 
 	using Node			  = typename BaseTypes::Node;
 	using NodePtr		  = typename BaseTypes::NodePtr;
-	//using NodeType		  = typename BaseTypes::node_type;
 	using difference_type = typename BaseTypes::difference_type;
-	using value_type	  = typename BaseTypes::value_type;
 	using pointer		  = typename BaseTypes::pointer;
 	using const_pointer	  = typename BaseTypes::const_pointer;
 	using reference		  = typename BaseTypes::reference;
 	using const_reference = typename BaseTypes::const_reference;
 
-	using key_type		= typename BaseTypes::key_type;
 	using key_compare	= typename Traits::key_compare;
 	using value_compare = typename Traits::value_compare;
 	using get_key		= typename Traits::get_key;
